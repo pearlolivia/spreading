ac95bdb6afe681ac819295b921dc573537379bbc
Pearl <57668822+pearlolivia@users.noreply.github.com>
2026-06-15 00:01:46 +0100

    generate dirt patches on enemy spawn
    (exacerbates "spreading")

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,6 +6,7 @@ var rng := RandomNumberGenerator.new()
 @onready var player: CharacterBody2D = $Player
 @onready var player_spawn_point = $EnemyControls/Spawner
 @onready var enemy_scene = preload("res://scenes/enemy.tscn")
+@onready var spawn_patch_scene = preload("res://scenes/spawn_patch.tscn")
 
 func _process(delta: float) -> void:
 	$Player/HUD/Health/HealthBar.value = player.health
@@ -16,10 +17,15 @@ func _on_spawn_timer_timeout() -> void:
 	var random_x = rng.randi_range(spawnerPosition.x - SPAWN_OFFSET, spawnerPosition.x + SPAWN_OFFSET)
 	var random_y = rng.randi_range(spawnerPosition.y - SPAWN_OFFSET, spawnerPosition.y + SPAWN_OFFSET)
 	var enemy = enemy_scene.instantiate()
+	var patch = spawn_patch_scene.instantiate()
 	
 	enemy.player = player
 	$Enemies.add_child(enemy)
 	enemy.position = Vector2(random_x, random_y)
+	
+	await get_tree().create_timer(0.5).timeout
+	$SpawnPatches.add_child(patch)
+	patch.position = Vector2(random_x, random_y + 8)
 
 func delete_enemies():
 	for enemy in $Enemies.get_children():
